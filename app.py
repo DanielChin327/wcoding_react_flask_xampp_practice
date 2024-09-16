5071f8c6d373bb8ffb47950d40a3c25f3d83a648
Daniel Chin <DanChin327@gmail.com>
2024-09-16 10:49:34 +0900

    practice

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import mysql.connector
+import sqlalchemy
 
 app = Flask(__name__)
 CORS(app)  # To enable cross-origin requests from the React frontend
+
+db = sqlalchemy.create_engine(
+        "mariadb+pymysql://root:@localhost:3307/famous_quotesdb", echo=True
+)
+
 
 # Configure MySQL connection
 db_config = {
@@ -14,15 +20,12 @@ db_config = {
     'database': 'famous_quotesdb'
 }
 
+
 # Testing routes
 @app.route('/hello')
 def home():
     return 'Hello There!'
 
-
-@app.route('/message')
-def home():
-    return 'This is the message page'
 
 # Route to handle saving quotes
 @app.route('/api/save_quote', methods=['POST'])
@@ -46,6 +49,28 @@ def save_quote():
         if connection.is_connected():
             cursor.close()
             connection.close()
+
+@app.route('/api/quotes', methods=['GET'])
+def get_quote():
+    try:
+        print("Attempting to connect to the database...")
+        with db.connect() as conn:
+            print("Connection successful!")
+            result = conn.execute(sqlalchemy.text('SELECT quote FROM quotes'))
+            quotes = [row['quote'] for row in result]
+            print("Fetched Quotes:", quotes)
+        return jsonify(quotes), 200
+    except Exception as e:
+        print("Error during database operation:", e)
+        return jsonify({'error': 'Failed to retrieve quotes'}), 500
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
